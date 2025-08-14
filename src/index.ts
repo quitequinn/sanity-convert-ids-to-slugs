da4edb22f81cebf4c36209f57681cf944473aefa
Quinn Keaveney <quinn@liiift.studio>
2025-08-14 11:20:31 -0700

    Add index file to export ConvertIdsToSlugs component

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Sanity Convert IDs to Slugs - Main Export
+ * Convert document IDs to human-readable slugs with customizable generation
+ */
+
+export { default as ConvertIdsToSlugs } from './ConvertIdsToSlugs'
+export { default } from './ConvertIdsToSlugs'
+
+// Export types if needed
+export type {
+  ConvertIdsToSlugsProps,
+  ConversionResult
+} from './ConvertIdsToSlugs'
